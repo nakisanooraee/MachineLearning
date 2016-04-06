a95aa5a74cdc2dfb1c9fd8b7b0815d674a9ad7d3
Nakisa Nooraee <nakisa.nooraee@gmail.com>
2016-04-06 19:17:48 +0200

    Create Versions.py

---
+++ python/Versions.py
@@ -0,0 +1,27 @@
+# python version
+import sys
+print('python    : {}'.format(sys.version))
+
+# scipy version
+import scipy
+print('scipy     : {}'.format(scipy.__version__))
+
+#numpy version
+import numpy 
+print('numpy     : {}'.format(numpy.__version__))
+
+#matplotlib version
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+
+# Pandas version 
+import pandas
+print('pandas    :{}'.format(pandas.__version__))
+
+# scikit-learn version 
+import sklearn
+print('sklearn   : {}'.format(sklearn.__version__))
+
+# seaborn version 
+import seaborn
+print('seaborn   :{}'.format(seaborn.__version__))
