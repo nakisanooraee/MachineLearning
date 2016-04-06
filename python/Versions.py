06a4b03d348535fcd22b3fefc6b86a1c3061193f
Nakisa Nooraee <nakisa.nooraee@gmail.com>
2016-04-06 19:20:33 +0200

    Update Versions.py

--- python/Versions.py
+++ python/Versions.py
@@ -1,29 +1,22 @@
 # This script prints the version of the different python libraries installed on the device.
 
-# python version
 import sys
 print('python    : {}'.format(sys.version))
 
-# scipy version
 import scipy
 print('scipy     : {}'.format(scipy.__version__))
 
-#numpy version
 import numpy 
 print('numpy     : {}'.format(numpy.__version__))
 
-#matplotlib version
 import matplotlib
 print('matplotlib: {}'.format(matplotlib.__version__))
 
-# Pandas version 
 import pandas
 print('pandas    :{}'.format(pandas.__version__))
 
-# scikit-learn version 
 import sklearn
 print('sklearn   : {}'.format(sklearn.__version__))
 
-# seaborn version 
 import seaborn
 print('seaborn   :{}'.format(seaborn.__version__))
